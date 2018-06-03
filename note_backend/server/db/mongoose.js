55d295df2089f9af28801e9a9c985701fb806d6f
Manuel Kruisz <kruisz88@gmail.com>
2018-06-03 18:27:23 +0200

    provide simple dockerfiles

--- note_backend/server/db/mongoose.js
+++ note_backend/server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/NoteApp');
+mongoose.connect('mongodb://127.0.0.1:27017/NoteApp');
 
 module.exports = {
     mongoose
